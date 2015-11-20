07288090df6e66a7289cdb0f56c9361f2a03e34f
talm8994 <tom.tom.pop13@gmail.com>
2015-11-20 12:57:37 -0700

    made it better

---
+++ app/src/main/java/ctec/videoandsound/controller/VideoActivity.java
@@ -0,0 +1,14 @@
+package ctec.videoandsound.controller;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.Menu
+
+/**
+ * Created by talm8994 on 11/12/15.
+ */
+public class VideoActivity
+{
+}
